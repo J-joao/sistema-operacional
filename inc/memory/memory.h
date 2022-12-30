db0aa262d934146eb5725e06842a5a1477863c2c
joao <83374960+J-joao@users.noreply.github.com>
2022-12-30 03:54:49 -0300

    Delete inc directory

--- inc/memory/memory.h
+++
@@ -1,9 +0,0 @@
-#ifndef MEMORY_H
-#define MEMORY_H
-
-#include <stddef.h>
-
-// essa função define os primeiros bytes (count) de "dest" para o valor "c"
-void *memset(void *ptr, int c, size_t size);
-
-#endif //MEMORY_H
